6155e8f5e1bb7cb9b7f9c153d4a33d4cc396d129
gosiarutkowska <malgorzatarutkowska2@gmail.com>
2023-08-27 17:46:03 +0200

    added unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,28 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Telecommunications Services Calculator', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('should render the main header', () => {
+    const headerElement = screen.getByText(/Kalkulator usług telekomunikacyjnych/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  test('should render the year selection dropdown', () => {
+    const selectElement = screen.getByText(/Wybierz rok usług/i);
+    expect(selectElement).toBeInTheDocument();
+  });
+
+  test('should render the services selection section', () => {
+    const serviceSelectionHeader = screen.getByText(/Wybierz usługi, którymi się interesujesz/i);
+    expect(serviceSelectionHeader).toBeInTheDocument();
+  });
+
+  test('should render the calculate button', () => {
+    const buttonElement = screen.getByRole('button', { name: /Oblicz wartość usług/i });
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
